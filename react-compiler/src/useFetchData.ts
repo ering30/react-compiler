501edd3036d2c5140a0d74ffd4a1e4aa4c3c127a
ering30 <115221464+ering30@users.noreply.github.com>
2025-11-16 20:48:04 +1100

    Create useFetchData.ts

---
+++ react-compiler/src/useFetchData.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export const useFetchData = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    console.log('inside useEffect')
+    fetch('/data.json')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return {
+    data
+  }
+}
