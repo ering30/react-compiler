f8415695fd7366d45ed19f449f1fce11a513e14e
ering30 <115221464+ering30@users.noreply.github.com>
2025-11-16 19:51:37 +1100

    remove unused imports

--- standard/src/App.tsx
+++ standard/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { useFetchData } from './useFetchData'
 import { Heading } from './components/Heading'
